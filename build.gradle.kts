a7f883e7d0711284873abbe6561e6e3d1c8a8ef9
Daniel <qiuzhanghua@icloud.com>
2020-09-11 07:45:07 +0800

    kotlin 1.4.0 -> 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.4.0"
+    kotlin("jvm") version "1.4.10"
     application
 }
 group = "com.example.talent.oct"
